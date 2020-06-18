226170e24f0be87bf5a688c90be1e9b9ddfdafb1
Subhoshri Ghosh <65813498+subhoshrighosh@users.noreply.github.com>
2020-06-18 20:09:10 +0530

    Create set.py

---
+++ Python-Task2/methods/set.py
@@ -0,0 +1,60 @@
+# working with sets
+
+
+# initialisation of sets 
+myset = {1,2,3,4,5,6,7,8,9}
+
+
+# maximum element in the set
+setmaximum = max(myset)					
+print(setmaximum)
+
+
+# minimum element in the set
+setminimum = min(myset)
+print(setminimum)
+
+
+# looping in sets
+for x in myset:
+    print(x)
+
+# length of the set
+setlength = len(myset)
+print(setlength)
+
+					
+# adding element in the list 
+myset.add(5)
+print(myset)
+
+
+# adding multiple elements in the list
+firstupdate = myset.update([34,67,65,87])
+print(myset)
+
+
+# copying the set
+nextset = myset.copy()
+print(nextset)
+
+
+
+nextset = {5,6,7,8,9}
+nextset_1 = {2,3,4,5,6}
+
+# intersection of the elements in the set
+mysetintersection = myset.intersection(nextset , nextset_1)
+print(mysetintersection)
+
+
+#difference of the elements in the set
+mysetdifference = myset.difference(nextset)	
+print(mysetdifference)
+
+
+# resulting the whole set
+unionSet = myset.union(nextset,nextset_1)
+print(unionSet)
+
+# Note :- Sets are immutable
